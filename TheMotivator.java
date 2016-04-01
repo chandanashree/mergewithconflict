693d8622e11a938d2bae405a7c11365d0689fdfc
chandana <chandanashree7@gmail.com>
2016-03-31 22:53:35 -0700

    modified getscore function

--- TheMotivator.java
+++ TheMotivator.java
@@ -4,12 +4,14 @@ public class TheMotivator {
 	public static void main(String[] args) {
 		System.out.println("Start");
 		TheMotivator tm = new TheMotivator();
-		ArrayList<Integer> scores = tm.getScore();
+		ArrayList<Integer> scores = tm.createScore();
 		System.out.println("Get Scores :"+scores);
 		
 		for(Integer score: scores) {
+			System.out.println("Score::"+score);
 			tm.printMessage(score);
 		}		
+		System.out.println("End");
 	}
 	public void printMessage(int score) {
 		if (score == 100)
@@ -18,6 +20,8 @@ public class TheMotivator {
 			System.out.println("That's great");
 		else if (score > 80)
 			System.out.println("That's good ");
+		else if (score > 70)
+			System.out.println("That's better ");
 		else
 			System.out.println("Well, what can I say?");
 	}
@@ -25,7 +29,7 @@ public class TheMotivator {
 	public void printScores(int score) {
 		System.out.println("Score::"+score);
 	}
-	public ArrayList<String> getScore(){
+	public ArrayList<String> createScore(){
 		ArrayList<String> scores  = new ArrayList<String>();
 		System.out.println("Get Individual scores");
 		scores.add(70);
