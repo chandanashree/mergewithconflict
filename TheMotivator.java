4f3820c5a32663b14beb652ebcd7084a97c3be81
chandana <chandanashree7@gmail.com>
2016-03-31 23:04:14 -0700

    modified printmessage function

--- TheMotivator.java
+++ TheMotivator.java
@@ -2,7 +2,7 @@ import java.util.ArrayList;
 
 public class TheMotivator {
 	public static void main(String[] args) {
-		System.out.println("Start");
+		System.out.println("Start Process");
 		TheMotivator tm = new TheMotivator();
 		ArrayList<Integer> scores = tm.getScore();
 		System.out.println("Get Scores :"+scores);
@@ -10,26 +10,28 @@ public class TheMotivator {
 		for(Integer score: scores) {
 			tm.printMessage(score);
 		}		
+		
+		System.out.println("End Process");
 	}
 	public void printMessage(int score) {
 		if (score == 100)
 			System.out.println("You're awesome");
 		else if (score > 90)
 			System.out.println("That's great");
+		else if (score > 85)
+			System.out.println("That's great");
 		else if (score > 80)
-			System.out.println("That's good ");
+			System.out.println("That's better ");
 		else
 			System.out.println("Well, what can I say?");
 	}
 	
-	public ArrayList<String> getScore(){
-		ArrayList<String> scores  = new ArrayList<String>();
-		System.out.println("Get Individual scores");
-		scores.add(70);
+	public ArrayList<Integer> getScore(){
+		ArrayList<Integer> scores  = new ArrayList<Integer>();
 		scores.add(60);
-		scores.add(30);
+		scores.add(70);
+		scores.add(80);
 		scores.add(90);
 		return scores;
-	}
-	
+	}	
 }
